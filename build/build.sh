01a1669de000ac3ff3584a6683c51664cfb61418
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2023-03-24 20:47:59 +0100

    ...

--- build/build.sh
+++ build/build.sh
@@ -1,1 +1,1 @@
-docker build  --no-cache ../ -f Dockerfile -t fluxms/medi-eco-book-stack-query-actions:v2022-03-22-2
+docker build  --no-cache ../ -f Dockerfile -t fluxms/medi-eco-book-stack-query-actions:v2022-03-24-1
